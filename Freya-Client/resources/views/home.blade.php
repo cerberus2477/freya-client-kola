e168dfc9419e1323cae3e87c415bcaa88725437a
3041TAN-13 <3041TAN-13@tanuzem.boronkay.local>
2025-02-18 12:13:34 +0100

    added articlecontroller and todo

--- Freya-Client/resources/views/home.blade.php
+++ Freya-Client/resources/views/home.blade.php
@@ -7,27 +7,28 @@ the image should look like a typical header image, full width, not repeating -->
     <h1 class="header-title">Freya's Garden</h1>
     <p class="header-text">catchphrase lorem ipsum dolor sit amet</p>
     <button class="btn btn-primary">Letöltés</button>
+    <button class="btn btn-green">Letöltés</button>
 </header>
 
 
-    <!-- this be should page-wide a strip, evenly spaced out -->
+<!-- this be should page-wide a strip, evenly spaced out -->
 <!-- number should be big, the description below that and smaller -->
-    <div class="accomplishments-strip">
-        <div>
-            <span>9999999</span> km3 <br>  Megtakarított Co2
-        </div>
-        <div>
-            <span>100.000</span> <br> Regisztrált felhasználó
-        </div>
-        <div>
-            <span>22.000</span> <br>Eladás/Csere
-        </div>
-        <div>
-            <span>numbers</span> <br> mmmmmhmmmmm fincsi
-        </div>
+<div class="accomplishments-strip">
+    <div>
+        <span>9999999</span> km3 <br> Megtakarított Co2
     </div>
-    
-    <main>
+    <div>
+        <span>100.000</span> <br> Regisztrált felhasználó
+    </div>
+    <div>
+        <span>22.000</span> <br>Eladás/Csere
+    </div>
+    <div>
+        <span>numbers</span> <br> mmmmmhmmmmm fincsi
+    </div>
+</div>
+
+<main>
     <!-- this should look almost like a table, with grid lines between, above and under the cards -->
     <div class="articles-strip">
         <div>
@@ -55,6 +56,13 @@ the image should look like a typical header image, full width, not repeating -->
             <img>
             <a class="btn">Olvass tovább <i class="fa-solid fa-chevron-right"></i></a>
         </div>
+        <!-- @foreach ($articles as $article)
+        <div class="card article">
+            <h3>{{ $article->title }}</h3>
+            <p>Növény neve</p>
+            <div class="content">{!! $article->content !!}</div> <!-- Render HTML -->
+    </div>
+    @endforeach -->
     </div>
 </main>
 
